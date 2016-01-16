6d6f828f7ce05627cfc0032916fb8f2f264680b3
Yanze LIU <yanzeliu.france@gmail.com>
2016-01-15 20:13:12 -0800

    Initial Commit

---
+++ OCR _Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCR _Test
+//
+//  Created by yanze on 1/15/16.
+//  Copyright © 2016 River Inn Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
